846f99f48a5da801c52ef8f7d4657dc2c3666257
ivan <ivannovaa.24@gmail.com>
2022-06-22 00:35:12 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+import { app } from './firebase'
 
 function App() {
+  const [user, setUser] = useState(null)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+
+    const auth = getAuth(app);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        setUser(user)
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+
+  }
+  console.log(user)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? `Selamat datang Admin | ${user.email}` :
+        <>
+          <h5>Firebase Login</h5>
+          <div className="login">
+            <input type="text" placeholder='email@domain.tld' onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleLogin}>Login</button>
+          </div>
+        </>}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3aE_eQwi3v9fbzLYLfNvulbKfT51vvLM",
+  authDomain: "tes-medium.firebaseapp.com",
+  projectId: "tes-medium",
+  storageBucket: "tes-medium.appspot.com",
+  messagingSenderId: "684926218097",
+  appId: "1:684926218097:web:18fa6e732b72b27d5c90d0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
