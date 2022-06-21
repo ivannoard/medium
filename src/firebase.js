0431d86576a72a9443e36702f54a34839e7fd62c
Ivan Ardiansyah <73393996+ivannoard@users.noreply.github.com>
2022-06-22 00:40:48 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
-  authDomain: "tes-medium.firebaseapp.com",
-  projectId: "tes-medium",
-  storageBucket: "tes-medium.appspot.com",
-  messagingSenderId: "684926218097",
-  appId: "1:684926218097:web:18fa6e732b72b27d5c90d0"
+  authDomain: "YOUR_AUTHDOMAIN",
+  projectId: "YOUR_PROJECTID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDERID",
+  appId: "YOUR_APP_ID"
 };
 
 // Initialize Firebase
